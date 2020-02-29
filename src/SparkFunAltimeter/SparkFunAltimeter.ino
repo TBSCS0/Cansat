062f90998412fe76ebb5627e834d21cc0d3a1452
Oscar Neiva <oscarneivaeneto@gmail.com>
2020-02-29 03:21:59 -0300

    reads temperature

---
+++ src/SparkFunAltimeter/SparkFunAltimeter.ino
@@ -0,0 +1,37 @@
+//https://learn.sparkfun.com/tutorials/mpl3115a2-pressure-sensor-hookup-guide
+
+#include <Wire.h>
+#include <SparkFunMPL3115A2.h>
+
+
+MPL3115A2 myPressure;
+
+
+void setup()
+{
+  Wire.begin();      
+  Serial.begin(9600);  
+
+  myPressure.begin(); 
+  myPressure.setModeAltimeter(); // Measure altitude above sea level in meters
+  myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
+  myPressure.setOversampleRate(128); // Set Oversample to the recommended 128
+  myPressure.enableEventFlags(); // Enable all three pressure and temp event flags 
+}
+
+void loop()
+{
+  float altitude = myPressure.readAltitude();
+  Serial.print("Altitude(m): ");
+  Serial.print(altitude, 2);
+
+  float pressure = myPressure.readPressure();
+  Serial.print("; Pressure(Pa): ");
+  Serial.print(pressure, 2);
+
+  float temperature = myPressure.readTemp();
+  Serial.print("; Temp(c):");
+  Serial.print(temperature, 2);
+
+  Serial.println();
+}
